cb6f0e50b388a659247665352655f3f5fbe0c58b
s3rcc <s3rcc.9@gmail.com>
2025-03-16 14:35:48 +0700

    token add day + preference response code

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -97,7 +97,7 @@ namespace Services
             SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddDays(7),
+                Expires = DateTime.UtcNow.AddHours(1),
                 Issuer = _configuration["Jwt:Issuer"],
                 Audience = _configuration["Jwt:Audience"],
                 SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
